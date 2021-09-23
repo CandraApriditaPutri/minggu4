8388890240b2137110bbe0ff1dff7b5724cde2c6
CandraApriditaPutri <candra.apridita@gmail.com>
2021-09-23 17:32:19 +0700

    controller: routes: view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <h1 align=center>Halaman Home</h1>
+        <h3 align=center>Menampilkan semua data dari tabel article yang ada di database minggu4</h3>
+        {{ $article }}
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [HomeController::class, 'getAll']);
+
+Route::get('/article/{id}', [ArticleController::class, 'getById']);
